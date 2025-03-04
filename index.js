8bad88cb0e4cfb1db036449ac90194c6254d1d00
Pauline Moraa <pmoraa431@gmail.com>
2025-03-05 02:26:16 +0300

    build while and for loop

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+ function writeCards(names, event){
+    let thankYouMessages = [];
+    for(let i =0; i<names.length; i++){
+        let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        thankYouMessages.push(message)
+    }
+    return thankYouMessages;
+ }
+
+ let cards = writeCards(["Polly", 'Daniel', 'Kylian'], 'wedding')
+ console.log(cards);
+
+ //ASSIGNMENT2
+ function countDown(countNumber){
+    let countDown = countNumber;
+
+    while (countDown >=0){
+       console.log(countDown);
+        countDown--;
+ }
+}
+countNumber(10)
